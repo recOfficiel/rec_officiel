313018f38a81189c9397b587f30028787c18c3e2
Sebastienlumuna <sebastienlumuna8@gmail.com>
2025-04-10 07:38:20 +0100

    creation du model et migration categories et annonces

---
+++ app/Models/Annonce.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Annonce extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'titre',
+        'slug',
+        'contenu',
+        'image',
+        'user_id',
+        'categorie_id',
+    ];
+}
