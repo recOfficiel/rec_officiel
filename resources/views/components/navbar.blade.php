92d2acb57cd108ec6a23854b75e95f8be0387784
Sebastienlumuna <sebastienlumuna8@gmail.com>
2025-04-16 15:04:03 +0100

    modification du navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -51,7 +51,12 @@
                         </div>
                     </div>
                     <a href="{{ route('contact')}}" class="nav-item nav-link @if (request()->is('/contact')) active @endif wow fadeInDown" data-wow-delay="0.1s" onmouseover="this.style.color='#3C3882'" onmouseout="this.style.color=''">Contact</a>
-                    <a href="{{ route('login_form')}}" class="nav-link me-3 wow fadeInDown fs-6 animate__animated animate__pulse animate__bounce" onmouseover="this.style.color='#3C3882'; this.classList.add('animate__headShake')" data-wow-delay="0.1s" onmouseout="this.style.color=''; this.classList.remove('animate__headShake')" style="margin-top: -8px;"><button class="btn btn-primary" style="background-color: #293070;"><i class="fas fa-user-circle fa-lg"></i> Se connecter</button></a>
+
+                    @auth
+                        <a href="" class="nav-link me-3 wow fadeInDown fs-6 animate__animated animate__pulse animate__bounce" onmouseover="this.style.color='#3C3882'; this.classList.add('animate__headShake')" data-wow-delay="0.1s" onmouseout="this.style.color=''; this.classList.remove('animate__headShake')" style="margin-top: -8px;"><button class="btn btn-primary" style="background-color: #293070;"><i class="fas fa-user-circle fa-lg"></i> Salut {{Auth::user()->name}} </button></a>
+                        @else
+                        <a href="{{ route('login_form')}}" class="nav-link me-3 wow fadeInDown fs-6 animate__animated animate__pulse animate__bounce" onmouseover="this.style.color='#3C3882'; this.classList.add('animate__headShake')" data-wow-delay="0.1s" onmouseout="this.style.color=''; this.classList.remove('animate__headShake')" style="margin-top: -8px;"><button class="btn btn-primary" style="background-color: #293070;"><i class="fas fa-user-circle fa-lg"></i> Se connecter</button></a>
+                        @endauth
 
                 </div>
 
