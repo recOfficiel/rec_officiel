839cf6fe68e8999162825011be88db98aeb16773
Sebastienlumuna <sebastienlumuna8@gmail.com>
2025-04-19 00:09:14 +0100

    creation du middleware roles pour la verification de roles

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        // verifaction si l'utilisateur est connecté
+        if (!auth()->check()) {
+            return redirect()->route('login');
+        }
+        // verifaction si l'utilisateur a le role
+        if (!auth()->user()->roles()->where('name', $role)->exists()) {
+            abort(403, 'Accès refusé');
+        }
+
+        return $next($request);
+    }
+}
